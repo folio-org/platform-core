5e671689a9e630f54bf3b3de8ff8cb9b01ea766c
Matt Jones <mattjones@ebsco.com>
2018-06-14 11:03:01 -0400

    Create test index for executing all tests

---
+++ test/ui-testing/test.js
@@ -0,0 +1,35 @@
+const startPage = require('./100-startpage');
+const authSuccess = require('./110-auth-success');
+const authFail = require('./120-auth-fail');
+const calendarTest = require('./calendarTest');
+const codexSearch = require('./codex-search');
+const dependencies = require('./dependencies');
+const exercise = require('./exercise');
+const inventorySearch = require('./inventorySearch');
+const loanRenewal = require('./loan_renewal');
+const location = require('./location');
+const profilePictures = require('./profile-pictures');
+const stub = require('./stub');
+const vendor = require('./vendor');
+
+
+module.exports.test = (uiTestCtx) => {
+  const allTests = [
+    startPage,
+    authSuccess,
+    authFail,
+    codexSearch,
+    dependencies,
+    exercise,
+    inventorySearch,
+    loanRenewal,
+    location,
+    profilePictures,
+    stub,
+    // TODO: Move these. They are not included in platform-core
+    // calendarTest,
+    // vendor
+  ];
+  
+  allTests.forEach(testModule => testModule.test(uiTestCtx));
+};
