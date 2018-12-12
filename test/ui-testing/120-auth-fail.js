a8aff27f1947f81b279eb23d42400481b467a110
ADITYA MATUKUMALLI <aditya.matukumalli@gmail.com>
2018-12-12 14:50:57 -0500

    fix lint in tests (#117)

--- test/ui-testing/120-auth-fail.js
+++ test/ui-testing/120-auth-fail.js
@@ -2,8 +2,6 @@ module.exports.test = (uiTestCtx, nightmare) => {
   describe('Login Page ("test-bad-login")', function test() {
     const { config } = uiTestCtx;
     this.timeout(Number(config.test_timeout));
-    
-
     describe('given bad data', () => {
       it('Should find a login error message', (done) => {
         nightmare
