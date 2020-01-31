f06709eca666735deeeb6e5f162c6a367903415a
Zak Burke <zburke@users.noreply.github.com>
2020-01-30 21:49:13 -0500

    FOLIO-2440 temp policies in exercise test (#524)
    Use helpers to add/remove overdue fine policy, lost item fee policy, and
    patron notice policy in the circulation rules that are created. Checkout
    is now smart enough to check all the attributes of circulation policies
    and if we just use dummy values, checkout will fail.
    
    Refs FOLIO-2440

--- test/ui-testing/exercise.js
+++ test/ui-testing/exercise.js
@@ -11,193 +11,230 @@ module.exports.test = (uiTestCtx) => {
     let openLoans = -1;
     let closedLoans = -1;
 
+    let initialRules = '';
+    const overdueFinePolicyName = `test-overdue-fine-policy-${Math.floor(Math.random() * 10000)}`;
+    const lostItemFeePolicyName = `test-lost-item-policy-${Math.floor(Math.random() * 10000)}`;
+    const noticePolicyName = `test-notice-policy-${Math.floor(Math.random() * 10000)}`;
+
     describe('Login > Update settings > Find user > Create inventory record > Create holdings record > Create item record > Checkout item > Confirm checkout > Checkin > Confirm checkin > Logout\n', function descStart() {
       it(`should login as ${config.username}/${config.password}`, (done) => {
         helpers.login(nightmare, config, done);
       });
 
-      it('should configure checkout for barcode and username', (done) => {
-        helpers.circSettingsCheckoutByBarcodeAndUsername(nightmare, config, done);
+      describe('it should configure settings', () => {
+        it('should navigate to settings', (done) => {
+          helpers.clickSettings(nightmare, done);
+        });
+
+        it('should configure checkout for barcode and username', (done) => {
+          helpers.circSettingsCheckoutByBarcodeAndUsername(nightmare, config, done);
+        });
+
+        describe('addOverdueFinePolicy', function foo() {
+          helpers.addOverdueFinePolicy(nightmare, overdueFinePolicyName);
+        });
+
+        describe('addLostItemFeePolicy', function foo() {
+          helpers.addLostItemFeePolicy(nightmare, lostItemFeePolicyName, 10, 'm');
+        });
+
+        describe('addNoticePolicy', function foo() {
+          helpers.addNoticePolicy(nightmare, noticePolicyName);
+        });
+
+        describe('it should configure circulation rules', function foo() {
+          it('set new rules circulation rules', (done) => {
+            const rules = `priority: t, s, c, b, a, m, g\nfallback-policy: l one-hour r hold-only n ${noticePolicyName} o ${overdueFinePolicyName} i ${lostItemFeePolicyName} \nm book: l example-loan-policy r allow-all n ${noticePolicyName} o ${overdueFinePolicyName} i ${lostItemFeePolicyName} `;
+            helpers.setCirculationRules(nightmare, rules)
+              .then(oldRules => {
+                initialRules = oldRules;
+              })
+              .then(done)
+              .catch(done);
+          });
+        });
       });
 
-      it('should navigate to settings', (done) => {
-        helpers.clickSettings(nightmare, done);
+      describe('exercise loans', function foo() {
+        it('should navigate to users', (done) => {
+          helpers.clickApp(nightmare, done, 'users');
+        });
+
+        it('should find an active user', (done) => {
+          helpers.findActiveUserBarcode(nightmare, 'faculty')
+            .then(bc => {
+              done();
+              console.log(`\t    Found user ${bc}`);
+              userBarcode = bc;
+            })
+            .catch(done);
+        });
+
+        it('should find current loans count', (done) => {
+          nightmare
+            .wait('#input-user-search')
+            .insert('#input-user-search', userBarcode)
+            .wait('#clickable-reset-all')
+            .click('#clickable-reset-all')
+            .insert('#input-user-search', userBarcode)
+            .wait('button[type=submit]')
+            .click('button[type=submit]')
+            .wait('#list-users[aria-rowcount="2"]')
+            .wait('#list-users a[role="row"][aria-rowindex="2"]')
+            .click('#list-users a[role="row"][aria-rowindex="2"]')
+            .wait('#clickable-viewcurrentloans')
+            .evaluate(() => document.querySelector('#clickable-viewcurrentloans').textContent)
+            .then((result) => {
+              openLoans = Number(result.replace(/^(\d+).*/, '$1'));
+              done();
+              console.log(`          Open loans: ${openLoans}`);
+            })
+            .catch(done);
+        });
+
+        it('should find closed loans count', (done) => {
+          nightmare
+            .wait(222)
+            .evaluate(() => document.querySelector('#clickable-viewclosedloans').textContent)
+            .then((result) => {
+              closedLoans = Number(result.replace(/^(\d+).*/, '$1'));
+              done();
+              console.log(`          Closed loans: ${closedLoans}`);
+            })
+            .catch(done);
+        });
+
+        const barcode = helpers.createInventory(nightmare, config, 'Soul station / Hank Mobley');
+
+        it('should navigate to checkout', (done) => {
+          helpers.clickApp(nightmare, done, 'checkout', 1000);
+        });
+
+        it(`should check out ${barcode}`, (done) => {
+          helpers.checkout(nightmare, done, barcode, userBarcode);
+        });
+
+        it('should navigate to users', (done) => {
+          helpers.clickApp(nightmare, done, 'users', 1000);
+        });
+
+        it('should change open-loan count', (done) => {
+          nightmare
+            .wait('#input-user-search')
+            .insert('#input-user-search', userBarcode)
+            .wait('#clickable-reset-all')
+            .click('#clickable-reset-all')
+            .insert('#input-user-search', userBarcode)
+            .wait('button[type=submit]')
+            .click('button[type=submit]')
+            .wait('#list-users[aria-rowcount="2"]')
+            .wait('#list-users a[role="row"][aria-rowindex="2"]')
+            .click('#list-users a[role="row"][aria-rowindex="2"]')
+            .wait('#clickable-viewcurrentloans')
+            .evaluate(() => document.querySelector('#clickable-viewcurrentloans').textContent)
+            .then((result) => {
+              if (Number(result.replace(/^(\d+).*/, '$1')) !== openLoans + 1) {
+                throw new Error('Open loan count did not change.');
+              }
+              done();
+            })
+            .catch(done);
+        });
+
+        it(`should find ${barcode} in open loans`, (done) => {
+          nightmare
+            .wait('#clickable-viewcurrentloans')
+            .click('#clickable-viewcurrentloans')
+            .wait(bc => {
+              return !!(Array.from(document.querySelectorAll('#list-loanshistory [role="gridcell"]'))
+                .find(e => e.textContent === `${bc}`));
+            }, barcode)
+            .wait('#users-module-display button[icon="times"][class*="iconButton"]')
+            .click('#users-module-display button[icon="times"][class*="iconButton"]')
+            .wait(parseInt(process.env.FOLIO_UI_DEBUG, 10) ? parseInt(config.debug_sleep, 10) : 555) // debugging
+            .then(done)
+            .catch(done);
+        });
+
+        it('should navigate to checkin', (done) => {
+          helpers.clickApp(nightmare, done, 'checkin');
+        });
+
+        it(`should check in ${barcode}`, (done) => {
+          nightmare
+            .wait('#input-item-barcode')
+            .insert('#input-item-barcode', barcode)
+            .wait('#clickable-add-item')
+            .click('#clickable-add-item')
+            .wait('#list-items-checked-in')
+            .wait(bc => {
+              return !!(Array.from(document.querySelectorAll('#list-items-checked-in [role="gridcell"]'))
+                .find(e => e.textContent === `${bc}`));
+            }, barcode)
+            .then(done)
+            .catch(done);
+        });
+
+        it('should navigate to users', (done) => {
+          helpers.clickApp(nightmare, done, 'users');
+        });
+
+        it('should change closed-loan count', (done) => {
+          nightmare
+            .wait('#pane-userdetails')
+            .wait('#clickable-viewclosedloans')
+            .evaluate(() => document.querySelector('#clickable-viewclosedloans').textContent)
+            .then((result) => {
+              if (Number(result.replace(/^(\d+).*/, '$1')) !== closedLoans + 1) {
+                throw new Error('Close loan count did not change.');
+              }
+              done();
+            })
+            .catch(done);
+        });
+
+        it(`should confirm ${barcode} in closed loans`, (done) => {
+          nightmare
+            .wait('#clickable-viewclosedloans')
+            .click('#clickable-viewclosedloans')
+            .wait(bc => {
+              return !!(Array.from(document.querySelectorAll('#list-loanshistory [role="gridcell"]'))
+                .find(e => e.textContent === `${bc}`));
+            }, barcode)
+            .wait('#users-module-display button[icon="times"][class*="iconButton"]')
+            .click('#users-module-display button[icon="times"][class*="iconButton"]')
+            .wait(parseInt(process.env.FOLIO_UI_DEBUG, 10) ? parseInt(config.debug_sleep, 10) : 555) // debugging
+            .then(done)
+            .catch(done);
+        });
       });
 
-      let initialRules = '';
+      describe('it should restore settings', () => {
+        it('should navigate to settings', (done) => {
+          helpers.clickSettings(nightmare, done);
+        });
 
-      it('should configure circulation rules', (done) => {
-        const rules = 'priority: t, s, c, b, a, m, g\nfallback-policy: l one-hour r hold-only n basic-notice-policy o test-overdue \nm book: l example-loan-policy r allow-all n alternate-notice-policy o test-overdue';
-        helpers.setCirculationRules(nightmare, rules)
-          .then(oldRules => {
-            initialRules = oldRules;
-          })
-          .then(done)
-          .catch(done);
+        it('should restore the circulation rules', (done) => {
+          helpers.setCirculationRules(nightmare, initialRules)
+            .then(() => done())
+            .catch(done);
+        });
+
+        describe('remove Overdue Fine Policy', function foo() {
+          helpers.removeOverdueFinePolicy(nightmare, overdueFinePolicyName);
+        });
+
+        describe('remove Lost Item Fee Policy', function foo() {
+          helpers.removeLostItemFeePolicy(nightmare, lostItemFeePolicyName);
+        });
+
+        describe('remove Notice Policy', function foo() {
+          helpers.removeNoticePolicy(nightmare, noticePolicyName);
+        });
       });
+    });
 
-      it('should navigate to users', (done) => {
-        helpers.clickApp(nightmare, done, 'users');
-      });
-
-      it('should find an active user', (done) => {
-        helpers.findActiveUserBarcode(nightmare, 'faculty')
-          .then(bc => {
-            done();
-            console.log(`\t    Found user ${bc}`);
-            userBarcode = bc;
-          })
-          .catch(done);
-      });
-
-      it('should find current loans count', (done) => {
-        nightmare
-          .wait('#input-user-search')
-          .insert('#input-user-search', userBarcode)
-          .wait('#clickable-reset-all')
-          .click('#clickable-reset-all')
-          .insert('#input-user-search', userBarcode)
-          .wait('button[type=submit]')
-          .click('button[type=submit]')
-          .wait('#list-users[aria-rowcount="2"]')
-          .wait('#list-users a[role="row"][aria-rowindex="2"]')
-          .click('#list-users a[role="row"][aria-rowindex="2"]')
-          .wait('#clickable-viewcurrentloans')
-          .evaluate(() => document.querySelector('#clickable-viewcurrentloans').textContent)
-          .then((result) => {
-            openLoans = Number(result.replace(/^(\d+).*/, '$1'));
-            done();
-            console.log(`          Open loans: ${openLoans}`);
-          })
-          .catch(done);
-      });
-
-      it('should find closed loans count', (done) => {
-        nightmare
-          .wait(222)
-          .evaluate(() => document.querySelector('#clickable-viewclosedloans').textContent)
-          .then((result) => {
-            closedLoans = Number(result.replace(/^(\d+).*/, '$1'));
-            done();
-            console.log(`          Closed loans: ${closedLoans}`);
-          })
-          .catch(done);
-      });
-
-      const barcode = helpers.createInventory(nightmare, config, 'Soul station / Hank Mobley');
-
-      it('should navigate to checkout', (done) => {
-        helpers.clickApp(nightmare, done, 'checkout', 1000);
-      });
-
-      it(`should check out ${barcode}`, (done) => {
-        helpers.checkout(nightmare, done, barcode, userBarcode);
-      });
-
-      it('should navigate to users', (done) => {
-        helpers.clickApp(nightmare, done, 'users', 1000);
-      });
-
-      it('should change open-loan count', (done) => {
-        nightmare
-          .wait('#input-user-search')
-          .insert('#input-user-search', userBarcode)
-          .wait('#clickable-reset-all')
-          .click('#clickable-reset-all')
-          .insert('#input-user-search', userBarcode)
-          .wait('button[type=submit]')
-          .click('button[type=submit]')
-          .wait('#list-users[aria-rowcount="2"]')
-          .wait('#list-users a[role="row"][aria-rowindex="2"]')
-          .click('#list-users a[role="row"][aria-rowindex="2"]')
-          .wait('#clickable-viewcurrentloans')
-          .evaluate(() => document.querySelector('#clickable-viewcurrentloans').textContent)
-          .then((result) => {
-            if (Number(result.replace(/^(\d+).*/, '$1')) !== openLoans + 1) {
-              throw new Error('Open loan count did not change.');
-            }
-            done();
-          })
-          .catch(done);
-      });
-
-      it(`should find ${barcode} in open loans`, (done) => {
-        nightmare
-          .wait('#clickable-viewcurrentloans')
-          .click('#clickable-viewcurrentloans')
-          .wait(bc => {
-            return !!(Array.from(document.querySelectorAll('#list-loanshistory [role="gridcell"]'))
-              .find(e => e.textContent === `${bc}`));
-          }, barcode)
-          .wait('#users-module-display button[icon="times"][class*="iconButton"]')
-          .click('#users-module-display button[icon="times"][class*="iconButton"]')
-          .wait(parseInt(process.env.FOLIO_UI_DEBUG, 10) ? parseInt(config.debug_sleep, 10) : 555) // debugging
-          .then(done)
-          .catch(done);
-      });
-
-      it('should navigate to checkin', (done) => {
-        helpers.clickApp(nightmare, done, 'checkin');
-      });
-
-      it(`should check in ${barcode}`, (done) => {
-        nightmare
-          .wait('#input-item-barcode')
-          .insert('#input-item-barcode', barcode)
-          .wait('#clickable-add-item')
-          .click('#clickable-add-item')
-          .wait('#list-items-checked-in')
-          .wait(bc => {
-            return !!(Array.from(document.querySelectorAll('#list-items-checked-in [role="gridcell"]'))
-              .find(e => e.textContent === `${bc}`));
-          }, barcode)
-          .then(done)
-          .catch(done);
-      });
-
-      it('should navigate to users', (done) => {
-        helpers.clickApp(nightmare, done, 'users');
-      });
-
-      it('should change closed-loan count', (done) => {
-        nightmare
-          .wait('#pane-userdetails')
-          .wait('#clickable-viewclosedloans')
-          .evaluate(() => document.querySelector('#clickable-viewclosedloans').textContent)
-          .then((result) => {
-            if (Number(result.replace(/^(\d+).*/, '$1')) !== closedLoans + 1) {
-              throw new Error('Close loan count did not change.');
-            }
-            done();
-          })
-          .catch(done);
-      });
-
-      it(`should confirm ${barcode} in closed loans`, (done) => {
-        nightmare
-          .wait('#clickable-viewclosedloans')
-          .click('#clickable-viewclosedloans')
-          .wait(bc => {
-            return !!(Array.from(document.querySelectorAll('#list-loanshistory [role="gridcell"]'))
-              .find(e => e.textContent === `${bc}`));
-          }, barcode)
-          .wait('#users-module-display button[icon="times"][class*="iconButton"]')
-          .click('#users-module-display button[icon="times"][class*="iconButton"]')
-          .wait(parseInt(process.env.FOLIO_UI_DEBUG, 10) ? parseInt(config.debug_sleep, 10) : 555) // debugging
-          .then(done)
-          .catch(done);
-      });
-
-      it('should navigate to settings', (done) => {
-        helpers.clickSettings(nightmare, done);
-      });
-
-      it('should restore the circulation rules', (done) => {
-        helpers.setCirculationRules(nightmare, initialRules)
-          .then(() => done())
-          .catch(done);
-      });
-
+    describe('it should log out', () => {
       it('should logout', (done) => {
         helpers.logout(nightmare, config, done);
       });
